f3d284453b180c79639401b98e2ec7778cb61148
rofaida4 <rofaidaabuzahra22@gmail.com>
2022-09-12 15:09:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-
-/**                                                                                                                         
-
- * main - Printing                                                                              
- *                                                                                                                           
- * Description: using the main function                                                                                            
- * Return: Always 0 (successful)                                                                                                              
- */                                                                                                                          
-int main(void)                                                                                                              	
-{                                                                             
-  puts("\"Programming is like building a multilingual puzzle");                   return (0);                                                           
-} 
+/**
+*main - Printing
+*
+*Description: using the main function
+*Return: Always 0 (successful)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
