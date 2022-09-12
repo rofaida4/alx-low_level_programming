1cbc57a950341aab35e6d59606910854953d0417
rofaida4 <rofaidaabuzahra22@gmail.com>
2022-09-12 14:46:27 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,10 @@
 
  * main - Printing                                                                              
  *                                                                                                                           
- * Description: using the main function                                                                                       
- * this program prints "Programming is like building a multilingual puzzle"                                                  
+ * Description: using the main function                                                                                            
  * Return: Always 0 (successful)                                                                                                              
  */                                                                                                                          
 int main(void)                                                                                                              	
- {                                                                             
-         puts("\"Programming is like building a multilingual puzzle");                   return (0);                                                           
-  } 
+{                                                                             
+  puts("\"Programming is like building a multilingual puzzle");                   return (0);                                                           
+} 
