2ecc1cdaaa7e01279d5cdeb87f8284c05124ed80
rofaida4 <rofaidaabuzahra22@gmail.com>
2022-09-12 12:57:42 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#inclode <stdio.h>
+/**
+
+ * main - entry point
+
+ * prints programming is like building
+
+ * multilingual puzzle, followed
+
+ *  by a new line
+
+ *
+
+ *  Return: 0
+*/
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+}
