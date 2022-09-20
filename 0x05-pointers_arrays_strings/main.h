3e7d9c012ca6b8e8f2149686fa761985f2829f60
rofaida4 <rofaidaabuzahra22@gmail.com>
2022-09-19 17:11:20 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * void prototypes()
+ * int prototypes()
+ */
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void _print_rev(char *s);
+void _rev_string(char *s);
+void _puts2(char *str);
+void _puts_half(char *str);
+void _print_array(int *a, int n);
+char *_strcpy(char *str);
+int _atoi(char *str);
+#endif /*MAIN_H*/
