fdedc2c456c89735c5a2ae7acfd3ba1f96f7ecb1
rofaida4 <rofaidaabuzahra22@gmail.com>
2022-10-04 14:23:01 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,8 +18,8 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+		str_concat[concat_index++] = s1[index];
 	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
-	return (concat_str);
+		str_concat[concat_index++] = s2[index];
+	return (str_concat);
 }
